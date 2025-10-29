a429deb2c45471c570e5ab7fb2eda04075b3854b
ppk321003 <ppk3210.kreasi03@gmail.com>
2025-10-29 15:14:46 +0700

    red

--- src/pages/BlockTanggal.tsx
+++ src/pages/BlockTanggal.tsx
@@ -1338,7 +1338,7 @@ export default function BlockTanggal() {
             {/* Input Kegiatan */}
             <div className="space-y-2">
               <label className="text-sm font-medium flex items-center gap-2">
-                <AlertCircle className="h-4 w-4 text-orange-500" />
+                <AlertCircle className="h-4 w-4 text-red-500" />
                 Nama Kegiatan
                 <span className="text-red-500">*</span>
               </label>
