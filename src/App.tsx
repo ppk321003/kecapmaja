bffa43a6c8f5f0738878cd5e237c832576cc8721
ppk321003 <ppk3210.kreasi03@gmail.com>
2025-11-02 08:11:00 +0700

    app

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,16 @@ import DownloadEDokumen from "./pages/e-dokumen/DownloadEDokumen";
 import PlaceholderEDokumen from "./pages/e-dokumen/PlaceholderEDokumen";
 import KerangkaAcuanKerja from "./pages/e-dokumen/kak";
 import DaftarHadir from "./pages/e-dokumen/daftar-hadir";
+import DokumenPengadaan from "./pages/e-dokumen/dokumen-pengadaan";
+import KuitansiPerjalananDinas from "./pages/e-dokumen/kuitansi-perjalanan";
+import KuitansiTransportLokal from "./pages/e-dokumen/kuitansi-transport";
+import LemburLaporan from "./pages/e-dokumen/lembur-laporan";
+import SPJHonor from "./pages/e-dokumen/spj-honor";
+import SuratKeputusan from "./pages/e-dokumen/surat-keputusan";
+import SuratPernyataan from "./pages/e-dokumen/spj-honor";
+import TandaTerima from "./pages/e-dokumen/tanda-terima";
+import TransportLokal from "./pages/e-dokumen/transport-lokal";
+import UangHarianTransport from "./pages/e-dokumen/uang-harian-transport";
 import EntriPetugas from "./pages/spk-bast/EntriPetugas";
 import EntriTarget from "./pages/spk-bast/EntriTarget";
 import EntriRealisasi from "./pages/spk-bast/EntriRealisasi";
@@ -51,17 +61,17 @@ const App = () => (
             <Route path="/e-dokumen/buat" element={<ProtectedRoute><Layout><BuatEDokumen /></Layout></ProtectedRoute>} />
             <Route path="/e-dokumen/download" element={<ProtectedRoute><Layout><DownloadEDokumen /></Layout></ProtectedRoute>} />
             <Route path="/e-dokumen/daftar-hadir" element={<ProtectedRoute><DaftarHadir /></ProtectedRoute>} />
-            <Route path="/e-dokumen/dokumen-pengadaan" element={<ProtectedRoute><Layout><PlaceholderEDokumen /></Layout></ProtectedRoute>} />
+            <Route path="/e-dokumen/dokumen-pengadaan" element={<ProtectedRoute><DokumenPengadaan /></ProtectedRoute>} />
             <Route path="/e-dokumen/kak" element={<ProtectedRoute><KerangkaAcuanKerja /></ProtectedRoute>} />
-            <Route path="/e-dokumen/kuitansi-perjalanan" element={<ProtectedRoute><Layout><PlaceholderEDokumen /></Layout></ProtectedRoute>} />
-            <Route path="/e-dokumen/kuitansi-transport" element={<ProtectedRoute><Layout><PlaceholderEDokumen /></Layout></ProtectedRoute>} />
-            <Route path="/e-dokumen/lembur-laporan" element={<ProtectedRoute><Layout><PlaceholderEDokumen /></Layout></ProtectedRoute>} />
-            <Route path="/e-dokumen/spj-honor" element={<ProtectedRoute><Layout><PlaceholderEDokumen /></Layout></ProtectedRoute>} />
-            <Route path="/e-dokumen/surat-keputusan" element={<ProtectedRoute><Layout><PlaceholderEDokumen /></Layout></ProtectedRoute>} />
-            <Route path="/e-dokumen/surat-pernyataan" element={<ProtectedRoute><Layout><PlaceholderEDokumen /></Layout></ProtectedRoute>} />
-            <Route path="/e-dokumen/tanda-terima" element={<ProtectedRoute><Layout><PlaceholderEDokumen /></Layout></ProtectedRoute>} />
-            <Route path="/e-dokumen/transport-lokal" element={<ProtectedRoute><Layout><PlaceholderEDokumen /></Layout></ProtectedRoute>} />
-            <Route path="/e-dokumen/uang-harian-transport" element={<ProtectedRoute><Layout><PlaceholderEDokumen /></Layout></ProtectedRoute>} />
+            <Route path="/e-dokumen/kuitansi-perjalanan" element={<ProtectedRoute><KuitansiPerjalananDinas /></ProtectedRoute>} />
+            <Route path="/e-dokumen/kuitansi-transport" element={<ProtectedRoute><KuitansiTransportLokal /></ProtectedRoute>} />
+            <Route path="/e-dokumen/lembur-laporan" element={<ProtectedRoute><LemburLaporan /></ProtectedRoute>} />
+            <Route path="/e-dokumen/spj-honor" element={<ProtectedRoute><SPJHonor /></ProtectedRoute>} />
+            <Route path="/e-dokumen/surat-keputusan" element={<ProtectedRoute><SuratKeputusan /></ProtectedRoute>} />
+            <Route path="/e-dokumen/surat-pernyataan" element={<ProtectedRoute><SuratPernyataan /></ProtectedRoute>} />
+            <Route path="/e-dokumen/tanda-terima" element={<ProtectedRoute><TandaTerima /></ProtectedRoute>} />
+            <Route path="/e-dokumen/transport-lokal" element={<ProtectedRoute><TransportLokal /></ProtectedRoute>} />
+            <Route path="/e-dokumen/uang-harian-transport" element={<ProtectedRoute><UangHarianTransport /></ProtectedRoute>} />
             <Route path="/spk-bast/entri-petugas" element={<ProtectedRoute><Layout><EntriPetugas /></Layout></ProtectedRoute>} />
             <Route path="/spk-bast/entri-target" element={<ProtectedRoute><Layout><EntriTarget /></Layout></ProtectedRoute>} />
             <Route path="/spk-bast/entri-realisasi" element={<ProtectedRoute><Layout><EntriRealisasi /></Layout></ProtectedRoute>} />
