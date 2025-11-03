9c3ac412c831b4893f88587bbd07bf2a509d2667
ppk321003 <ppk3210.kreasi03@gmail.com>
2025-11-03 18:08:18 +0700

    scr

--- src/components/ui/scroll-area.tsx
+++ src/components/ui/scroll-area.tsx
@@ -15,39 +15,18 @@ const ScrollArea = React.forwardRef<
     <ScrollAreaPrimitive.Viewport 
       className={cn(
         "h-full w-full rounded-[inherit]",
-        // CSS untuk menyembunyikan scrollbar di berbagai browser
-        "[&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]"
+        // Hide scrollbar for all browsers
+        "[&::-webkit-scrollbar]:hidden",
+        "[-ms-overflow-style:none]",
+        "[scrollbar-width:none]"
       )}
     >
       {children}
     </ScrollAreaPrimitive.Viewport>
-    {/* ScrollBar dihilangkan untuk menyembunyikan sepenuhnya */}
     <ScrollAreaPrimitive.Corner />
   </ScrollAreaPrimitive.Root>
 ));
 ScrollArea.displayName = ScrollAreaPrimitive.Root.displayName;
 
-// Komponen ScrollBar tidak diekspor atau dibuat invisible
-const ScrollBar = React.forwardRef<
-  React.ElementRef<typeof ScrollAreaPrimitive.ScrollAreaScrollbar>,
-  React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.ScrollAreaScrollbar>
->(({ className, orientation = "vertical", ...props }, ref) => (
-  <ScrollAreaPrimitive.ScrollAreaScrollbar
-    ref={ref}
-    orientation={orientation}
-    className={cn(
-      "flex touch-none select-none transition-colors",
-      orientation === "vertical" &&
-        "h-full w-0 border-l border-l-transparent p-[1px]", // width diubah menjadi 0
-      orientation === "horizontal" &&
-        "h-0 flex-col border-t border-t-transparent p-[1px]", // height diubah menjadi 0
-      className
-    )}
-    {...props}
-  >
-    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-transparent" />
-  </ScrollAreaPrimitive.ScrollAreaScrollbar>
-));
-ScrollBar.displayName = ScrollAreaPrimitive.ScrollAreaScrollbar.displayName;
-
-export { ScrollArea, ScrollBar };
+// Hanya export ScrollArea saja
+export { ScrollArea };
