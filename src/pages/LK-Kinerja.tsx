5b3f5a96c2eb852d84151b5614fa51954f686827
ppk321003 <ppk3210.kreasi03@gmail.com>
2025-11-25 17:11:30 +0700

    text-center

--- src/pages/LK-Kinerja.tsx
+++ src/pages/LK-Kinerja.tsx
@@ -380,7 +380,7 @@ export default function LKKinerja() {
       {/* Dashboard Pegawai Terbaik */}
       <div className="grid grid-cols-1 lg:grid-cols-5 gap-6">
         {/* Pegawai Terbaik Tahun 2025 */}
-        <Card className="lg:col-span-2 bg-gradient-to-br from-yellow-50 to-amber-100 border-amber-200">
+        <Card className="lg:col-span-2 bg-gradient-to-br from-yellow-50 to-amber-100 border-amber-200 text-center">
           <CardHeader className="pb-3">
             <CardTitle className="flex items-center gap-2 text-amber-800">
               <Trophy className="h-6 w-6" />
