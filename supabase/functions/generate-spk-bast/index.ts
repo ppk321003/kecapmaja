d884ad4e2bfd3bc3f36fdde435a74ad1c6d644f9
Pengadaan 3210 <pengadaan3210@gmail.com>
2026-04-13 18:07:02 +0700

    1

--- supabase/functions/generate-spk-bast/index.ts
+++ supabase/functions/generate-spk-bast/index.ts
@@ -11,7 +11,7 @@ serve(async (req: Request) => {
       headers: {
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        "Access-Control-Allow-Headers": "authorization, x-client-info, apikey, content-type",
       },
     });
   }
